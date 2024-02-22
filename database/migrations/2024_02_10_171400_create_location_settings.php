10406d4e89e0dc8415b09d307d516cc8c4aeb134
51mpLeDev <firdavs02shukurov@gmail.com>
2024-02-22 20:29:32 +0500

    add migration

---
+++ database/migrations/2024_02_10_171400_create_location_settings.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Firdavs\OwnersDashboard\Database\Migrations;
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationSettings extends Migration
+{
+    public function up()
+    {
+        if (Schema::hasTable('location_settings')){
+            return;
+        }
+        Schema::create('location_settings', function ($table){
+            $table->increments('id')->unsigned();
+            $table->integer('settings_id')->nulable();
+            $table->integer('location_id')->nulable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('location_settings');
+    }
+}
