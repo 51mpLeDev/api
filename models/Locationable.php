0176af2d00b342e260992e46408dafddc67d6fef
51mpLeDev <firdavs02shukurov@gmail.com>
2024-02-23 19:22:44 +0500

    add Locatinable model

---
+++ models/Locationable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Firdavs\Api\Models;
+
+use Admin\Models\Tables_model;
+use Illuminate\Database\Eloquent\Model;
+
+class Locationable extends Model
+{
+    protected $table = 'locationables';
+
+    public function getLocationTables($id)
+    {
+        $ids = $this->where('location_id', $id)->where('locationable_type', 'tables')->get('id')->toArray();
+        return Tables_model::whereIn('table_id', $ids)->get();
+    }
+}
