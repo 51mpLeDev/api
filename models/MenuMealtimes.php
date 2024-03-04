0f7c490d13582f00c7ecf140b4875893598ed7ea
51mpLeDev <firdavs02shukurov@gmail.com>
2024-03-04 14:24:29 +0500

    add MenuMEaltimes model

---
+++ models/MenuMealtimes.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Firdavs\Api\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MenuMealtimes extends Model
+{
+    protected $table = 'menu_mealtimes';
+
+    public $timestamps = false;
+
+    protected $guarded = false;
+}
