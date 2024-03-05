e31b7664afec0447f8ab730f109ed2a831934509
51mpLeDev <firdavs02shukurov@gmail.com>
2024-03-05 19:12:12 +0500

    add RolesUser

---
+++ models/RolesUser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Firdavs\Api\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RolesUser extends Model
+{
+    public $timestamps = false;
+    public $table = 'roles_users';
+}
