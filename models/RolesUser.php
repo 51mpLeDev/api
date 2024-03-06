6880df89ef1f1e52673e44582dd9404bfcd301c0
51mpLeDev <firdavs02shukurov@gmail.com>
2024-03-06 16:42:09 +0500

    update RolesUser model

--- models/RolesUser.php
+++ models/RolesUser.php
@@ -2,10 +2,20 @@
 
 namespace Firdavs\Api\Models;
 
+use Admin\Models\Users_model;
 use Illuminate\Database\Eloquent\Model;
+use System\Models\Roles_model;
 
 class RolesUser extends Model
 {
     public $timestamps = false;
     public $table = 'roles_users';
+    protected $guarded = false;
+
+    public function role(){
+        return $this->belongsTo(Roles_model::class, 'role_id', 'id');
+    }
+    public function user(){
+        return $this->belongsTo(Users_model::class, 'user_id', 'user_id');
+    }
 }
