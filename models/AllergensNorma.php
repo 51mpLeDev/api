2646ec87c3caa3777e7c852a73742c49be11bd43
51mpLeDev <firdavs02shukurov@gmail.com>
2024-03-23 17:03:08 +0500

    update AllergansNorma

--- models/AllergensNorma.php
+++ models/AllergensNorma.php
@@ -2,6 +2,7 @@
 
 namespace Firdavs\Api\Models;
 
+use Admin\Models\Allergens_model;
 use Illuminate\Database\Eloquent\Model;
 
 class AllergensNorma extends Model
@@ -9,4 +10,9 @@ class AllergensNorma extends Model
     protected $table = 'allergens_norma';
     public $timestamps = false;
     protected $primaryKey = 'allergens_norma_id';
+
+    public function allergen()
+    {
+        return $this->belongsTo(Allergens_model::class, 'allergen_id', 'allergen_id');
+    }
 }
