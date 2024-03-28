edcdf5a63b9765aac6a1ec5e2612568c8bbe0ea1
51mpLeDev <firdavs02shukurov@gmail.com>
2024-03-28 22:18:26 +0500

    add order tip column

---
+++ database/migrations/2024_28_03_1798746_add_order_tip_column_to_orders_table.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Firdavs\Api\Database\Migrations;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOrderTipColumnToOrdersTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table){
+            $table->string('order_tip')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table){
+            $table->dropColumn('order_tip');
+        });
+    }
+}
