6b9184bdd103e9618703fb5055fb1bcba2754e22
51mpLeDev <firdavs02shukurov@gmail.com>
2024-03-29 17:39:14 +0500

    add parent id to orders

---
+++ database/migrations/2024_03_29_46464_add_parent_id_and_grand_total_columns_to_orders_table.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Firdavs\Api\Database\Migrations;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddParentIdAndGrandTotalColumnToOrdersTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table){
+            $table->integer('parent_id')->default(0);
+            $table->string('grand_total')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table){
+            $table->dropColumn(['parent_id', 'grand_total']);
+        });
+    }
+}
