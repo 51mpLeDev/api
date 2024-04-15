d52c9b614aed2d12fd332793b9fadb4efbd9703f
51mpLeDev <firdavs02shukurov@gmail.com>
2024-04-15 16:17:33 +0500

    update Locationable model

--- models/Locationable.php
+++ models/Locationable.php
@@ -2,6 +2,7 @@
 
 namespace Firdavs\Api\Models;
 
+use Admin\Models\Menus_model;
 use Admin\Models\Tables_model;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,5 +16,11 @@ class Locationable extends Model
     {
         $ids = static::where('location_id', $id)->where('locationable_type', 'tables')->get('locationable_id')->toArray();
         return Tables_model::whereIn('table_id', $ids)->get();
+    }
+
+    public static function getLocationMenus($id)
+    {
+        $ids = static::where('location_id', $id)->where('locationable_type', 'menus')->get('locationable_id')->toArray();
+        return Menus_model::where('menu_id', $ids)->get();
     }
 }
